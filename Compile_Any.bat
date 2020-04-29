77041bb556bcc06e88bfef9d12aaff40264fbeec
Ensiform <ensiform@gmail.com>
2020-04-29 18:45:15 -0500

    Initial commit

---
+++ Compile_Any.bat
@@ -0,0 +1,4 @@
+"MY:\PATH\TO\ETF\q3map_2.5.16_win32_x86\q3map2.exe" -fs_game etf -custinfoparms -meta -v -fs_basepath "MY:\PATH\TO\ET" -game et "C:\Games\ETF\etf\maps\MAPNAME.map"
+"MY:\PATH\TO\ETF\q3map_2.5.16_win32_x86\q3map2.exe" -fs_game etf -vis -saveprt -v -fs_basepath "MY:\PATH\TO\ET" -game et "C:\Games\ETF\etf\maps\MAPNAME.bsp"
+"MY:\PATH\TO\ETF\q3map_2.5.16_win32_x86\q3map2.exe" -fs_game etf -light -bounce 8 -q3 -fast -patchshadows -samples 2 -v -fs_basepath "MY:\PATH\TO\ET" -game et "C:\Games\ETF\etf\maps\MAPNAME.bsp"
+pause
---
+++ compile-any.bat
@@ -0,0 +1,3 @@
+<PATH TO Q3MAP2> -fs_basepath <PATH TO ET FOLDER> -fs_game "etf" -game "et" -custinfoparms -meta -v <MAPNAME>
+<PATH TO Q3MAP2> -fs_basepath <PATH TO ET FOLDER> -fs_game "etf" -game "et" -vis -saveprt <MAPNAME>
+<PATH TO Q3MAP2> -fs_basepath <PATH TO ET FOLDER> -fs_game "etf" -game "et" -light -q3 -fast -samples 2 -bounce 8 -patchshadows -v <MAPNAME>
